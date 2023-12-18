a217341949be4a4d3e530c37baf15d09d72fa5f8
Philip Clarke <clarkep19@gmail.com>
2023-12-18 18:08:09 -0500

    delete bad stuff

--- webScrapeIntro.py
+++ webScrapeIntro.py
@@ -16,9 +16,6 @@ season_table = soup.find('table', id="stats")
 
 #with open('testgameData.csv', 'w') as f:
 
-# we love fortnite
-# i love CP
-
 ## open and write to testGameData.csv
 f = open('testGameData.csv', 'w', newline='')
 scrapedGames = csv.writer(f)
